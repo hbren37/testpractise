e0ea9cceaa09709cbf0de4daba46a2be5b3b4ed4
Helen Brennan <9981703@student.toiohomai.ac.nz>
2018-08-29 16:59:20 +1200

    testpractise1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("---------Question 1-----------");
+            Console.WriteLine("-----------------------------");
+
+
+            Console.WriteLine(" Please enter a letter and I will tell you whether its a vowel or not");
+            string letter = Console.ReadLine();
+
+
+
+            switch (letter)
+            {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+
+
+
+                    
+                    Console.WriteLine("That is a vowel");
+                    break;
+                default:
+                    Console.WriteLine("{0} is a consonant");
+                    break; }
+
+            Console.WriteLine("Please press enter to exit");
+        
+
+        
+         Console.WriteLine("------------Question 2-----------");
+         Console.WriteLine("This app will calculate your BMI (Body mass index)");
+            Console.WriteLine("Please enter your weight in Kg's:");
+            int weight = int.Parse(Console.ReadLine());
+
+
+
+
+
+                       
+                    
+         
+        
+
+   
+
+        
+
+        
+
+           
+        }
+    }
+}
